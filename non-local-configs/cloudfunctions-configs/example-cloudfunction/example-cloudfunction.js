bae430ace02359b4639a6228af57c98908194f9b
d <d@ms.com>
2019-05-15 12:52:46 +0800

    修正规格
    Signed-off-by: d <d@ms.com>

--- non-local-configs/cloudfunctions-configs/example-cloudfunction/example-cloudfunction.js
+++ non-local-configs/cloudfunctions-configs/example-cloudfunction/example-cloudfunction.js
@@ -2,6 +2,8 @@
 
 // 部署：在 cloud-functions/${cloudfunction-name} 文件夹右击选择 “上传并部署”
 
+
+// 配置方式一：源码写法
 /**
  * 云函数的功能性说明, eg. 获得用户的唯一标识符
  * 
@@ -14,4 +16,22 @@ exports.main = (event, context) => {
   return {
     openid: event.userInfo.openId,
   }
+}
+
+// 配置方式二：接口写法(推荐)
+/* 调用方式 */
+wx.cloud.callFunction({
+  name: '${cloudfunction-name}',
+  data: {},
+  success: res => { },
+  fail: err => { }
+})
+
+/* 返回值参考 */
+return {
+  openid: event.userInfo.openId,
+}
+// 或直接给出例值
+{
+  openid: "123456789ascd"
 }
