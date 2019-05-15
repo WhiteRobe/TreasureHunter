964bf692424c6cedc7a11c0141e5a0398cfa62c1
wang jiawei <1327769133@qq.com>
2019-05-15 20:21:26 +0800

    获取openid和经纬度已经实现

--- miniprogram/pages/gamebegin/gamebegin.js
+++ miniprogram/pages/gamebegin/gamebegin.js
@@ -1,66 +1,117 @@
-// miniprogram/pages/gamebegin/gamebegin.js
+// pages/gamebegin/gamebegin
+const common = require('../../mod/modules/common.js')
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    imageAddress: '../../resources/images/pikaqiu.jpg',
+  },
 
+  /**
+   * 创建一局游戏
+   */
+  createGame: function() {
+    //获取用户ID
+    common.getOpenid()
+      .then(res => {
+        let openid = res;
+        console.log(openid);
+        common.getLocation()
+          .then(res => {
+            console.log('纬度', res.latitude, '经度', res.longitude)
+            let latitude = res.latitude;
+            let longitude = res.longitude;
+            console.log('纬度', latitude, '经度', longitude);
+            wx.navigateTo({
+              url: '/pages/creategame/creategame?openid=' + openid + '&latitude=' + latitude + '&longitude=' + longitude
+            })
+          })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+
+  },
+  /**
+   * 加入一局游戏
+   */
+  joinGame: function() {
+    //获取用户ID
+    common.getOpenid()
+      .then(res => {
+        let openid = res;
+        console.log(openid);
+        common.getLocation()
+          .then(res => {
+            console.log('纬度', res.latitude, '经度', res.longitude)
+            let latitude = res.latitude;
+            let longitude = res.longitude;
+            console.log('纬度', latitude, '经度', longitude);
+            wx.navigateTo({
+              url: '/pages/ingame/ingame?openid=' + openid + '&latitude=' + latitude + '&longitude=' + longitude
+            })
+          })
+      })
+      .catch(err => {
+        console.log(err)
+      })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
